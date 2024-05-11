1c440025f12287d5dc1f817ac8071255c6c4a896
Bunyamin Kalkan <bnkalkan41@gmail.com>
2024-05-11 12:21:30 +0300

    feat: add strong password validation

--- api/src/main/java/com/bunyaminkalkan/api/requests/UserUpdateRequest.java
+++ api/src/main/java/com/bunyaminkalkan/api/requests/UserUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.bunyaminkalkan.api.requests;
 
 import com.bunyaminkalkan.api.annotations.UniqueUserName;
+import com.bunyaminkalkan.api.annotations.ValidPassword;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +12,7 @@ public class UserUpdateRequest {
     @UniqueUserName
     private String userName;
     private String currentPassword;
+    @ValidPassword
     private String newPassword;
     private String confirmNewPassword;
     @Email(message = "Please enter a valid email address.")
