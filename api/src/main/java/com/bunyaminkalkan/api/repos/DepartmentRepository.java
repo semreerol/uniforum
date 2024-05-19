7866219472083fcb2dbc7f70e83503b1d661b06c
Bunyamin Kalkan <bnkalkan41@gmail.com>
2024-05-19 17:24:18 +0300

    feat: add deployment and data initializer

--- api/src/main/java/com/bunyaminkalkan/api/repos/DepartmentRepository.java
+++ api/src/main/java/com/bunyaminkalkan/api/repos/DepartmentRepository.java
@@ -4,4 +4,7 @@ import com.bunyaminkalkan.api.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    boolean existsByName(String facultyOfLaw);
+
+    Department findByName(String computerEngineering);
 }
--- api/src/main/java/com/bunyaminkalkan/api/repos/UniversityRepository.java
+++ api/src/main/java/com/bunyaminkalkan/api/repos/UniversityRepository.java
@@ -4,4 +4,5 @@ import com.bunyaminkalkan.api.entities.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UniversityRepository extends JpaRepository<University, Long> {
+    boolean existsByName(String trakyaUniversity);
 }
